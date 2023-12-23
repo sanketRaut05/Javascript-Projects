c0345d5c17355327de380fbd3cf5ba132d07e4d3
sanketRaut05 <sanketRautips@email.com>
2023-12-23 18:21:00 +0530

    newone

---
+++ E-commerce/js/form.js
@@ -0,0 +1,15 @@
+var loginform= document.getElementById('loginform');
+var regform= document.getElementById('regform');
+var indicator=document.getElementById('indicator');
+function register(){
+    regform.style.transform='translateX(0px)';
+    loginform.style.transform='translateX(0px)';
+    indicator.style.transform='translateX(100px)';
+
+}
+function login(){
+    regform.style.transform='translateX(300px)';
+    loginform.style.transform='translateX(300px)';
+    indicator.style.transform='translateX(0px)';
+
+}
